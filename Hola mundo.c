24e144899b233f9f5c7851a7adefc9bb14804c3c
EMBFCFM <60374176+EMBFCFM@users.noreply.github.com>
2020-01-27 21:08:31 -0600

    Add files via upload

---
+++ Hola mundo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include<stdio.h>
+int main(){
+
+printf("Hola mundo");
+printf("Mi nombre es Ramon Alberto Zapata Cruz y mi matricula es 1848881");
+return 0;
+
+}
