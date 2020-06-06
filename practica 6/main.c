9d18905d55cc09e0546687c08cdb2221aa2c0c36
Ramon Alberto Zapata Cruz <60374176+EMBFCFM@users.noreply.github.com>
2020-06-05 19:56:09 -0500

    Add files via upload
    practica 6

---
+++ practica 6/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int n1,n2,i;
+	float n=0;
+	printf("Bienvenido\n");
+	printf("Ingrese un numero mayor o igual que cero, en cambio ingrese un numero negativo para terminar:\n");
+	scanf("%d", &n1);
+	do{
+		printf("\n\nIngrese un numero mayor o igual que cero, en cambio ingrese un numero negativo para terminar\n");
+		scanf("%d",&n1);
+		n2=n2+n1;
+		i++;
+	}while(n1>=0);
+	n= n2/i;
+	printf("El promedio es: %.2f\n",n);
+	return 0;
+
+}
